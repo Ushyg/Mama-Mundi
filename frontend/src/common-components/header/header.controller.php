042003e377742343423c3d79ca180a8607772431
Ushyg <marquet.art92@gmail.com>
2017-11-23 11:44:51 +0100

    [stable state] refacto header + organization

--- frontend/src/common-components/header/header.controller.php
+++ frontend/src/common-components/header/header.controller.php
@@ -1,24 +1,4 @@
 <?php
-$headerCtrl = new HeaderController($menu_items, $icons);
-
-	Class HeaderController{
-		private $_menuItems;
-		private $_icons;
-
-		function __contruct ($menu_items, $icons){
-			$this->_menuItems = $menu_items;
-			$this->_icons = $icons;
-		}
-
-		function get MenuItems(){
-			return $this->_menuItems;
-		}
-
-		function get icons(){
-			return $this->_icons
-		}
-
-	}
 
 $menu_items = array(
 	array ("label" => "Acceuil", "url" => "./index.php"),
@@ -28,21 +8,21 @@ $menu_items = array(
 		"childs" => array(
 			array("label" => "Notre organisation", "url" => "#"),
 			array("label" => "Notre mission", "url" => "#"),
-			array("label" => "Nos projets", "url" => "#")
+			array("label" => "Nos projets", "url" => "#"),
 			array("label" => "Notre équipe au sénégal", "url" => "#")
 		)
-	)
+	),
 	array (
 		"label" => "Nos activités", 
 		"url" => "./activite.php", 
 		"childs" => array(
 			array("label" => "Education", "url" => "#"),
 			array("label" => "Formation professionnelle", "url" => "#"),
-			array("label" => "Soutien aux familles", "url" => "#")
-			array("label" => "Nos résultats", "url" => "#")
+			array("label" => "Soutien aux familles", "url" => "#"),
+			array("label" => "Nos résultats", "url" => "#"),
 			array("label" => "Matériel éducatif", "url" => "#")
 		)
-	)
+	),
 	array (
 		"label" => "Nous soutenir", 
 		"url" => "#", 
@@ -61,4 +41,25 @@ $icons = array(
 	array("src-img" => "images/twitter.svg", "alt-img" => "image bandeau", "href" => "#"),
 	array("src-img" => "images/youtube.svg", "alt_img" => "image bandeau", "href" => "https://www.youtube.com/channel/UC1LRzm-VfgE9KHXJpKcjiLQ")
 );
+
+$headerCtrl = new HeaderController($menu_items, $icons);
+
+	Class HeaderController{
+		private $_menuItems;
+		private $_icons;
+
+		function __construct ($menu_items, $icons){
+			$this->_menuItems = $menu_items;
+			$this->_icons = $icons;
+		}
+
+		function getMenuItems(){
+			return $this->_menuItems;
+		}
+
+		function getIcons(){
+			return $this->_icons;
+		}
+
+	}
 ?>
