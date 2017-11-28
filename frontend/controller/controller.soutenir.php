a8e8031d59a9912ee61aa14dc51bd493e8943110
DeDreux <32632354+DeDreux@users.noreply.github.com>
2017-11-28 17:06:28 +0100

    controller/view soutiens

---
+++ frontend/controller/controller.soutenir.php
@@ -0,0 +1,14 @@
+<?php
+include_once ('./view/common-components/head/view_head.php');
+include_once ('./view/common-components/header/view_header.php');
+
+if(isset($_GET['parrainer'])){
+    include_once ('./view/contents/view.parrainer.php');
+} else if(isset($_GET['don'])){
+    include_once ('./view/contents/view.don.php');
+} else if(isset($_GET['soutenir'])){
+    include_once ('./view/contents/view.benevole.php');
+} else {
+    include_once ('./view/contents/view.soutenir.php');
+}
+?>
--- frontend/view/contents/view.parrainer.php
+++ frontend/view/contents/view.parrainer.php
@@ -1,65 +1,5 @@
-<!DOCTYPE html>
-	<html>
-		<head>
-			<meta charset="utf-8">
-			<title> Mama mundi </title>
-			<link rel="stylesheet" href="css/styles.css"/>
-			<link rel="stylesheet" href="css/form_par.css"/>
-			<link rel="stylesheet" href="css/menuder.css"/>
-			<script type="text/javascript" src="menu.js"></script>	
-		</head>
-		<body background="images/back1.jpg">
-			<div id="navbar">
-				<div id="bandeau">
-					<a href="index.php"> <img src="images/Logo1.png" alt="image bandeau"/></a>
-					<h1>Mama Mundi</h1>
-					<div id="icones">
-						<img src="images/france.svg" alt="image bandeau"/>
-						<img src="images/spain.svg" alt="image bandeau"/>
-						<a href="https://www.facebook.com/asociacion.jaimerever/?ref=page_internal"><img src="images/facebook.svg" alt="image bandeau"/></a>
-						<img src="images/twitter.svg" alt="image bandeau"/>
-						<a href="https://www.youtube.com/channel/UC1LRzm-VfgE9KHXJpKcjiLQ"><img src="images/youtube.svg" alt="image bandeau"/></a>
-					</div>
-				</div>
-				<div id="menu">
-					<ul id="menu_der">
-						<li>
-							<a id="liens" href="index.php" title="Page d'acceuil">Accueil</a>
-						</li>
-						<li>
-							<a id="liens" href="asso.php" title="Page d'acceuil">L’association</a>
-								<ul>
-	                        		<li><a href="#">Notre organisation</a></li>
-	                        		<li><a href="#">Notre mission</a></li>
-	                        		<li><a href="#map">Nos projets</a></li>
-	                        		<li><a href="#">Notre équipe au Sénégal</a></li>
-	                			</ul>
-						</li>
-						<li>
-							<a id="liens" href="activite.php" title="Page actualités">Nos activités</a>
-								<ul>
-	                        		<li><a href="#">Éducation</a></li>
-	                        		<li><a href="#">Formation professionnelle</a></li>
-	                        		<li><a href="#">Soutien aux familles</a></li>
-	                        		<li><a href="#">Nos résultats</a></li>
-	                        		<li><a href="#">Matériel éducatif</a></li>
-	                			</ul>
-						</li>
-						<li>
-							<a id="liens" title="Page formulaire">Nous soutenir</a>
-								<ul>
-	                        		<li><a href="parrainer.php">Parrainer</a></li>
-	                        		<li><a href="don.php">Faire un don</a></li>
-	                        		<li><a href="benevole.php">Devenir bénévole</a></li>
-	                			</ul>
-						</li>
-					</ul>
-				</div>
-			</div>
-			</div>
-			<div id=espace2>
-			</div>
-		<section id="formule">
+<link rel="stylesheet" href="./css/form_par.css"/>
+<section id="formule" style="margin-top:25%;">
 			<div id="ssform">	
 			<div id="coordo">
 	        	<h2>Mes coordonnées</h2>
@@ -183,33 +123,4 @@ function getNBjour(){
 	}
 }
 </script>
-
-<!--
-function initcbs(){
-		var cbjour = document.getElementsByName('cbjour')[0];
-		var cbmois = document.getElementsByName('cbmois')[0];
-		var cbannees = document.getElementsByName('cbannees')[0];
-
-		for(var j=0; j<31; j++){
-			var joption= new Option();
-			joption.value= j;
-			joption.text= j+1;
-			cbjour.add(joption);
-		}
-
-		var tab=Array('janvier','fevrier','mars','avril','mai','juin','juillet','aout','septembre','octobre','novembre','décembre');
-		for(var m=0; m<12; m++){
-		var moption= new Option();
-		moption.value= tab[m];
-		moption.text= tab[m];
-		cbmois.add(moption);
-		}
-
-		for(var a=1985; a<2021; a++){
-			var aoption= new Option();
-			aoption.value= a;
-			aoption.text= a;
-			cbannees.add(aoption);
-		}
-}
 
