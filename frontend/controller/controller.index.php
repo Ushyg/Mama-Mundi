f0c3944aa13e3646983796515773292846f6921b
Val <valentin-vic@orange.fr>
2017-11-30 15:09:59 +0100

    essai

--- frontend/controller/controller.index.php
+++ frontend/controller/controller.index.php
@@ -2,4 +2,5 @@
 include_once ('./view/common-components/head/view_head.php');
 include_once ('./view/common-components/header/view_header.php');
 include_once ('./view/contents/view.index.php');
+include_once ('./view/common-components/footer/view_footer.php');
 ?>
--- frontend/view/common-components/head/view_head.php
+++ frontend/view/common-components/head/view_head.php
@@ -10,6 +10,7 @@
 	<title> Mama mundi </title>
 	<link rel="stylesheet" href="./css/styles.css"/>
 	<link rel="stylesheet" href="./css/menuder.css"/>
+	<link rel="stylesheet" href="./css/view_footer.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
