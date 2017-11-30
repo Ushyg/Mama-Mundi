b9a902b256289dee16e637937405e862a100d25f
xu-maranello <carremaxence@gmail.com>
2017-11-30 10:36:11 +0100

    responsive Index + Menu_Der (mais pas sous-menu)

--- frontend/view/common-components/header/view_header.php
+++ frontend/view/common-components/header/view_header.php
@@ -4,50 +4,62 @@
 <body background="images/back1.jpg">
 <!--______________________Menu______________________ -->
 
-<div id="navbar">
-    <div id="bandeau">
-        <a href="index.php"> <img src="./images/Logo1.png" alt="image bandeau"/></a>
-        <h1>Mama Mundi</h1>
-        <div id="icones">
-            <img src="./images/france.svg" alt="image bandeau"/>
-            <img src="./images/spain.svg" alt="image bandeau"/>
-            <a href="https://www.facebook.com/asociacion.jaimerever/?ref=page_internal"><img src="images/facebook.svg" alt="image bandeau"/></a>
-            <img src="./images/twitter.svg" alt="image bandeau"/>
-            <a href="https://www.youtube.com/channel/UC1LRzm-VfgE9KHXJpKcjiLQ"><img src="./images/youtube.svg" alt="image bandeau"/></a>
+    <div id="navbar">
+        <div id="bandeau" class="row">
+            <div class="col-xs-4 text-left">
+                <a href="index.php"> <img src="./images/Logo1.png" alt="image bandeau"/></a>
+            </div>
+            <div class="col-xs-4 text-center">                
+                <h2>Mama Mundi</h2>
+            </div>
+            <div id="icones" class="col-xs-4 text-right">
+                <img src="./images/france.svg" alt="image bandeau"/>
+                <img src="./images/spain.svg" alt="image bandeau"/>
+                <a href="https://www.facebook.com/asociacion.jaimerever/?ref=page_internal"><img src="images/facebook.svg" alt="image bandeau"/></a>
+                <img src="./images/twitter.svg" alt="image bandeau"/>
+                <a href="https://www.youtube.com/channel/UC1LRzm-VfgE9KHXJpKcjiLQ"><img src="./images/youtube.svg" alt="image bandeau"/></a>
+            </div>
         </div>
+        <nav id="menu" class="row">
+            <ul id="menu_der" class="col-xs-offset-0 col-xs-12 col-md-offset-1 col-md-10 text-center">
+                <li class="col-xs-3 text-center">
+                    <a id="liens" href="./index.php" title="Page d'acceuil">Accueil</a>
+                </li>
+                <li class="col-xs-3 text-center">
+                    <a id="liens" href="./asso.php" title="Page d'acceuil">L’association</a>
+                        <ul class="">
+                            <li class="">
+                                <a href="#">Notre organisation</a>
+                            </li>
+                            <li class="">
+                                <a href="#">Notre mission</a>
+                            </li>
+                            <li class="">
+                                <a href="#map">Nos projets</a>
+                            </li>
+                            <li class="">
+                                <a href="#">Notre équipe au Sénégal</a>
+                            </li>
+                        </ul>
+                </li>
+                <li class="col-xs-3 text-center">
+                    <a id="liens" href="activite.php" title="Page actualités">Nos activités</a>
+                        <ul class="col-xs-3">
+                            <li><a href="#">Éducation</a></li>
+                            <li><a href="#">Formation professionnelle</a></li>
+                            <li><a href="#">Soutien aux familles</a></li>
+                            <li><a href="#">Nos résultats</a></li>
+                            <li><a href="#">Matériel éducatif</a></li>
+                        </ul>
+                </li>
+                <li class="col-xs-3 text-center">
+                    <a id="liens" title="Page formulaire">Nous soutenir</a>
+                        <ul class="col-xs-3">
+                            <li><a href="./parrainer.php">Parrainer</a></li>
+                            <li><a href="./don.php">Faire un don</a></li>
+                            <li><a href="./benevole.php">Devenir bénévole</a></li>
+                        </ul>
+                </li>
+            </ul>
+        </nav>
     </div>
-    <div id="menu">
-        <ul id="menu_der">
-            <li>
-                <a id="liens" href="./index.php" title="Page d'acceuil">Accueil</a>
-            </li>
-            <li>
-                <a id="liens" href="./asso.php" title="Page d'acceuil">L’association</a>
-                    <ul>
-                        <li><a href="#">Notre organisation</a></li>
-                        <li><a href="#">Notre mission</a></li>
-                        <li><a href="#map">Nos projets</a></li>
-                        <li><a href="#">Notre équipe au Sénégal</a></li>
-                    </ul>
-            </li>
-            <li>
-                <a id="liens" href="activite.php" title="Page actualités">Nos activités</a>
-                    <ul>
-                        <li><a href="#">Éducation</a></li>
-                        <li><a href="#">Formation professionnelle</a></li>
-                        <li><a href="#">Soutien aux familles</a></li>
-                        <li><a href="#">Nos résultats</a></li>
-                        <li><a href="#">Matériel éducatif</a></li>
-                    </ul>
-            </li>
-            <li>
-                <a id="liens" title="Page formulaire">Nous soutenir</a>
-                    <ul>
-                        <li><a href="./parrainer.php">Parrainer</a></li>
-                        <li><a href="./don.php">Faire un don</a></li>
-                        <li><a href="./benevole.php">Devenir bénévole</a></li>
-                    </ul>
-            </li>
-        </ul>
-    </div>
-</div>
